5f429af55293e9a8dd026479fa9f1d3277e0966f
Wayne Yong <ymw0331@gmail.com>
2022-11-23 12:20:15 +0800

    add paint clone

---
+++ 018 splash-page/.history/script_20221123100259.js
@@ -0,0 +1,30 @@
+const { body } = document;
+
+const ourMemeory = {
+  
+}
+
+
+function changeBackground(number) {
+  // Check if background is aldready showing
+  let previousBackground;
+  if (body.className) {
+    previousBackground = body.className;
+  }
+  // Reset css class on body
+  body.className = "";
+  switch (number) {
+    case "1":
+      return previousBackground === "background-1"
+        ? false
+        : body.classList.add("background-1");
+    case "2":
+      return previousBackground === "background-2"
+        ? false
+        : body.classList.add("background-2");
+    case "3":
+      return previousBackground === "background-3"
+        ? false
+        : body.classList.add("background-3");
+  }
+}
---
+++ 018 splash-page/.history/script_20221123100403.js
@@ -0,0 +1,31 @@
+const { body } = document;
+let count = 0;
+const ourMemeory = {
+  [count]: Array(10000).fill("*"),
+};
+
+function changeBackground(number) {
+  count++;
+  ourMemeory[count]= Array(10000).fill()
+  // Check if background is aldready showing
+  let previousBackground;
+  if (body.className) {
+    previousBackground = body.className;
+  }
+  // Reset css class on body
+  body.className = "";
+  switch (number) {
+    case "1":
+      return previousBackground === "background-1"
+        ? false
+        : body.classList.add("background-1");
+    case "2":
+      return previousBackground === "background-2"
+        ? false
+        : body.classList.add("background-2");
+    case "3":
+      return previousBackground === "background-3"
+        ? false
+        : body.classList.add("background-3");
+  }
+}
